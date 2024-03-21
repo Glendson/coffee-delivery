1162e9efaa37b3c3addfe96e1e6d939b4d9b129e
Glendson <gztomazetto@gmail.com>
2024-03-21 13:15:49 +0000

    feat: added routes on app and Header component

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,7 +1,25 @@
+import { NavLink } from "react-router-dom";
 import { HeaderContainer } from "./styles";
+import Logo from "../../../public/logo.svg";
+import { MapPin, ShoppingCart } from "@phosphor-icons/react";
 
-export function Header(){
-    return (
-        <HeaderContainer>Cabecalho</HeaderContainer>
-    )
+export function Header() {
+  return (
+    <HeaderContainer>
+      <NavLink to="/">
+        <img src={Logo} />
+      </NavLink>
+
+      <aside>
+        <div>
+          <MapPin width={22} weight="fill" />
+          <span>Paranavaí, PR</span>
+        </div>
+
+        <NavLink to="/checkout">
+          <ShoppingCart width={22} weight="fill" />
+        </NavLink>
+      </aside>
+    </HeaderContainer>
+  );
 }
--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -1,3 +1,51 @@
 import styled from "styled-components";
 
-export const HeaderContainer = styled.header``
+export const HeaderContainer = styled.header`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+
+  max-width: 72.5rem;
+  padding: 32px 160px;  
+  
+  aside {
+    display: flex;
+    align-items: center;
+    gap: 0.75rem;
+
+    div {
+      display: flex;
+      align-items: center;
+      gap: 0.25rem;
+
+      padding: 10px 8px;
+
+      border-radius: 0.375rem;
+
+      background: ${(props) => props.theme.colors["purple-light"]};
+
+      svg {
+        color: ${(props) => props.theme.colors["purple-dark"]};
+      }
+
+      span {
+        text-size: ${(props) => props.theme.fonts.textS};
+        color: ${(props) => props.theme.colors["purple-dark"]}
+      }
+    }
+
+    a {
+      display: flex;
+      align-items: center;
+      height: 2.375rem;
+
+      border-radius: 0.375rem;
+      gap: 0.25rem;
+      padding: 8px;
+      position: relative;
+
+      background: ${(props) => props.theme.colors["yellow-light"]};
+      color: ${(props) => props.theme.colors["yellow-dark"]};
+    }
+  }  
+`;
