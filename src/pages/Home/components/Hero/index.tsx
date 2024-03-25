c72e5df0883c231d1bb1ab8b68a1868b052a6b4f
Glendson <gztomazetto@gmail.com>
2024-03-25 19:34:58 +0000

    feat: created Hero component for home page

--- src/pages/Home/components/Hero/index.tsx
+++ src/pages/Home/components/Hero/index.tsx
@@ -1,7 +1,47 @@
-import { HeroContainer } from "./styles";
+import { HeroContainer, HeroContent, HeroFeatures } from "./styles";
+import heroBg from "../../../../../public/assets/hero-bg.svg";
+import hero from "../../../../../public/assets/hero.svg";
+import { Coffee, Package, ShoppingCart, Timer } from "@phosphor-icons/react";
 
-export function Hero(){
-    return (
-        <HeroContainer>Content</HeroContainer>
-    )
+export function Hero() {
+  return (
+    <HeroContainer>
+      <HeroContent>
+        <div>
+          <header>
+            <h1>Encontre o café perfeito para qualquer hora do dia</h1>
+
+            <span>
+              Com o Coffee Delivery você recebe seu café onde estiver, a
+              qualquer hora
+            </span>
+          </header>
+
+          <HeroFeatures>
+            <div>
+              <ShoppingCart width={16} weight="fill" />
+              <span>Compra simples e segura.</span>
+            </div>
+
+            <div>
+              <Package width={16} weight="fill" />
+              <span>Embalagem mantém o café intacto</span>
+            </div>
+
+            <div>
+              <Timer width={16} weight="fill" />
+              <span>Entrega rápida e rastreada</span>
+            </div>
+
+            <div>
+              <Coffee width={16} weight="fill" />
+              <span>O café chega fresquinho até você</span>
+            </div>
+          </HeroFeatures>
+        </div>
+        <img src={hero} id="hero" alt="" />
+      </HeroContent>
+      <img src={heroBg} id="hero-bg" alt="" />
+    </HeroContainer>
+  );
 }
--- src/pages/Home/components/Hero/styles.ts
+++ src/pages/Home/components/Hero/styles.ts
@@ -1,3 +1,73 @@
 import styled from "styled-components";
+import { defaultTheme } from "../../../../styles/themes/default"
 
-export const HeroContainer = styled.div``
+interface HeroFeaturesProps {
+    $color?: keyof typeof defaultTheme.colors;
+}
+
+export const HeroContainer = styled.section`
+  position: relative;
+
+  header {
+
+    display: flex;
+    flex-direction: column;
+    gap: 1rem;
+
+    h1{
+        font: ${props => props.theme.fonts.titleXL};
+        color: ${props => props.theme.colors["base-title"]};
+    }
+    span {
+        font: ${props => props.theme.fonts.textL};
+        color: ${props => props.theme.colors["base-subtitle"]};
+    }
+  }
+
+  img#hero-bg {
+    position: absolute;
+    top: 0;
+    left: 0;
+    max-height: 544px;
+    width: 100vw;
+    object-fit: cover;
+  }
+`;
+
+export const HeroContent = styled.div`
+  max-width: 1160px;
+  padding: 92px 20px;
+  margin: 0 auto;
+
+  display: flex;
+  gap: 56px;
+  align-items: flex-start;
+  justify-content: space-between;
+
+  > div {
+    display: flex;
+    flex-direction: column;
+    gap: 66px;
+  }
+  
+`;
+
+export const HeroFeatures = styled.div<HeroFeaturesProps>`
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  grid-row-gap: 20px;
+
+  div {
+    display: flex;
+    align-items: center;
+    gap: 12px;
+    padding: 0;
+
+    svg {
+      padding: 8;
+      border-radius: 50%;
+      color: ${props => props.theme.colors.background};
+      background: ${props => props.theme.colors[props.$color || "purple"]};
+    }
+  }
+`;
