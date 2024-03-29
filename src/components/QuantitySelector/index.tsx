f13f860fb49ef0f2b733e036e2afc6a6758ecd3f
Glendson <gztomazetto@gmail.com>
2024-03-29 15:54:33 +0000

    feat: created Quantity selector component

--- src/components/QuantitySelector/index.tsx
+++ src/components/QuantitySelector/index.tsx
@@ -1,11 +1,26 @@
+import { Minus, Plus } from "@phosphor-icons/react";
 import { QuantityContainer } from "./styles";
 
-export function QuantitySelector(){
-    return (
-        <QuantityContainer>
-            <span>+</span>
-            <span>1</span>
-            <span>-</span>
-        </QuantityContainer>
-    )
+interface QuantitySelectorProps {
+  quantity: number;
+  inclementQuantity: () => void;
+  declementQuantity: () => void;
+}
+
+export function QuantitySelector({
+  quantity,
+  inclementQuantity,
+  declementQuantity,
+}: QuantitySelectorProps) {
+  return (
+    <QuantityContainer>
+      <button onClick={inclementQuantity}>
+        <Plus />
+      </button>
+      <span>{quantity}</span>
+      <button onClick={declementQuantity}>
+        <Minus />
+      </button>
+    </QuantityContainer>
+  );
 }
--- src/components/QuantitySelector/styles.ts
+++ src/components/QuantitySelector/styles.ts
@@ -1,3 +1,37 @@
 import { styled } from "styled-components";
 
-export const QuantityContainer = styled.div``
+export const QuantityContainer = styled.div`
+  width: 4.5rem;
+  height: 2.375rem;
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  gap: 0.25rem;
+  padding: 0.5rem;
+  border-radius: 0.375rem;
+
+  background: ${(props) => props.theme.colors["base-button"]};
+
+  button {
+    width: 0.875rem;
+    height: 0.875rem;
+    display: flex;
+    align-items: center;
+    border: 0;
+    color: ${(props) => props.theme.colors["purple"]};
+    background: ${(props) => props.theme.colors["base-button"]};
+
+    transition: all 0.2s;
+
+    &:hover {
+      color: ${(props) => props.theme.colors["purple-dark"]};
+    }
+  }
+  
+  span {
+    font: ${(props) => props.theme.fonts.textM };
+    color: ${(props) => props.theme.colors["base-title"]};
+  }
+`;
