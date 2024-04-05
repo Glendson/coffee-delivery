1bb9ae6c2c3d1f848407f98d22418c7b23aaa1b3
Glendson <gztomazetto@gmail.com>
2024-04-05 16:53:22 +0100

    feat: starting to style the Checkout page

--- src/pages/Checkout/index.tsx
+++ src/pages/Checkout/index.tsx
@@ -1,9 +1,68 @@
-import { CheckoutContainer } from "./styles";
+import {
+  MapPinLine,
+  CurrencyDollar,
+  CreditCard,
+  Bank,
+  Money,
+} from "@phosphor-icons/react";
+import {
+  AddressContainer,
+  AddressFormContainer,
+  AddressHeader,
+  CheckoutContainer,
+  OrderCompleteContainer,
+  OrderSelectContainer,
+  PaymentContainer,
+  PaymentMethods,
+  PaymentButton,
+} from "./styles";
 
-export function Checkout(){
-    return (
-        <CheckoutContainer>
-            <h1>Checkout</h1>
-        </CheckoutContainer>
-    )
+export function Checkout() {
+  return (
+    <CheckoutContainer>
+      <OrderCompleteContainer>
+        <h2>Complete o seu Pedido</h2>
+
+        <form action="">
+          <AddressContainer>
+            <AddressHeader>
+              <MapPinLine />
+              <div>
+                <span>Endereço de Entrega</span>
+
+                <p>Informe o endereço onde deseja receber seu pedido</p>
+              </div>
+            </AddressHeader>
+            <AddressFormContainer></AddressFormContainer>
+          </AddressContainer>
+          <PaymentContainer>
+            <div>
+              <CurrencyDollar />
+              <span>Pagamento</span>
+
+              <p>
+                O pagamento é feito na entrega. Escolha a forma que deseja pagar
+              </p>
+            </div>
+
+            <PaymentMethods>
+              <PaymentButton>
+                <CreditCard size={32} />
+                <span>Cartão de crédito</span>
+              </PaymentButton>
+              <PaymentButton>
+                <Bank size={32} />
+                <span>cartão de débito</span>
+              </PaymentButton>
+              <PaymentButton>
+                <Money size={32} />
+                <span>dinheiro</span>
+              </PaymentButton>
+            </PaymentMethods>
+          </PaymentContainer>
+        </form>
+      </OrderCompleteContainer>
+      <OrderSelectContainer></OrderSelectContainer>
+    </CheckoutContainer>
+  );
 }
--- src/pages/Checkout/styles.ts
+++ src/pages/Checkout/styles.ts
@@ -1,3 +1,81 @@
 import styled from "styled-components";
 
-export const CheckoutContainer = styled.div``
+export const CheckoutContainer = styled.div``;
+
+export const OrderCompleteContainer = styled.div`
+  width: 40rem;
+
+  display: flex;
+  flex-direction: column;
+  align-items: left;
+  justify-content: center;
+  gap: 0.75rem;
+  padding: 0;
+  border-radius: 0.375rem;
+`;
+
+export const AddressContainer = styled.div`
+  flex: 1;
+  gap: 2rem;
+  padding: 2.5rem;
+
+  background: ${(props) => props.theme.colors["base-card"]};
+`;
+
+export const AddressHeader = styled.div``;
+
+export const AddressFormContainer = styled.div``;
+
+const BaseAddressInput = styled.input`
+  height: 2.625rem;
+  padding: 0.75rem;
+  border: 0;
+  border-radius: 0.25rem;
+  font: ${(props) => props.theme.fonts.textS};
+  color: ${(props) => props.theme.colors["base-label"]};
+  background: ${(props) => props.theme.colors["base-input"]};
+`;
+
+export const AddressCepInput = styled(BaseAddressInput)`
+  width: 12.5rem;
+`;
+
+export const AddressInput = styled(BaseAddressInput)`
+  flex: 1;
+`;
+
+export const AddressLineGroup = styled.div`
+  display: flex;
+  gap: 0.75rem;
+`;
+
+export const PaymentContainer = styled.div`
+  flex: 1;
+  border-radius: 0.375rem;
+  display: flex;
+  gap: 2rem;
+  padding: 2.5rem;
+
+  background: ${(props) => props.theme.colors["base-card"]};
+`;
+
+export const PaymentMethods = styled.div``;
+
+export const PaymentButton = styled.button`
+  display: flex;
+  gap: 0.75rem;
+  border-radius: 0.375rem;
+  padding: 1rem;
+
+  font: ${(props) => props.theme.fonts.buttonM};
+  color: ${(props) => props.theme.colors["base-text"]};
+  background: ${(props) => props.theme.colors["base-button"]};
+
+  svg {
+    width: 1rem;
+    height: 1rem;
+    color: ${(props) => props.theme.colors.purple};
+  }
+`;
+
+export const OrderSelectContainer = styled.div``;
